6e5a68ea5cf3d6f9cea23d484c122df8fdb63bdd
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2023-01-05 00:29:41 -0500

    change httpAdapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,7 +3,7 @@ import { PokeResponse } from './interfaces/poke-response.interrface';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { FetchAdapter } from 'src/common/httpAdapters/fetch.adapter';
+import { AxiosAdapter } from 'src/common/httpAdapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
@@ -13,7 +13,7 @@ export class SeedService {
     @InjectModel(Pokemon.name) // Inyectamos el modelo en nest
     private readonly pokemonModel: Model<Pokemon>,
 
-    private readonly http: FetchAdapter // Adaptador de http (AxiosAdapter o FetchAdapter)
+    private readonly http: AxiosAdapter // Adaptador de http (AxiosAdapter o FetchAdapter)
     
   ) { }
 
